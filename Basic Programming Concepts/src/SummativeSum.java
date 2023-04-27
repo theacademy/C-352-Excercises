10b9fe33cf30fe7753eef440b8dac5a658271093
spunkymandar <59153449+spunkymandar@users.noreply.github.com>
2023-04-27 21:43:59 +0530

    Add files via upload

---
+++ Basic Programming Concepts/src/SummativeSum.java
@@ -0,0 +1,12 @@
+
+public class SummativeSum {
+
+	public static void main(String[] args) {
+		//addArrayElements()
+		
+	}
+	private static int addArrayElements(int[] myarra) {
+		return 0;
+	}
+
+}
