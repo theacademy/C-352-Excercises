c25782ef0d072b30ed5cd6bc0e2bab6c5e6ec2e2
spunkymandar <59153449+spunkymandar@users.noreply.github.com>
2023-05-15 10:21:11 +0530

    Add files via upload

---
+++ Mandatory_Assessment_JavaClassObjects/src/com/model/DVD.java
@@ -0,0 +1,13 @@
+package com.model;
+
+import java.util.Date;
+
+public class DVD {
+	String title;
+	Date releaseDate;
+	String mpAArating;
+	String directorName;
+	String studioName;
+	String userRating;
+	
+}
